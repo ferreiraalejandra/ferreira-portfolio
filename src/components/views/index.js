1a4942387ac6b7b8cce672bf7d82865155b16cf6
ferreiraalejandra <ferreiratorres.alejandra@gmail.com>
2020-05-11 13:00:25 +0200

    adding navigation using react router to the app

---
+++ src/components/views/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Home from './Home'
+import About from './About'
+import Projects from './Projects'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Views = () => {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default Views
