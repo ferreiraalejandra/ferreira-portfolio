97e8376771269ddc93070d22a87a8b7e593b3f92
ferreiraalejandra <ferreiratorres.alejandra@gmail.com>
2020-05-12 01:09:10 +0200

    styling and adding text content on about and projects pages

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -5,8 +5,9 @@ const Home = () => {
   return (
     <>
       <section className='content-section'>
-        Alejandra ferreira with hero Image
         <div className='hero-image' style={{backgroundImage: `url(${heroImage})`}} >
+        <div className='title-container'><span>Ale Ferreira</span></div>
+        <div className='hero-text-container'><span>Full Stack Web Developer<br />Agile enthusiast<br />Driven autodidact<br />Co-founder @MagentaCodes</span></div>
         </div>
       </section>
       <section className='content-section'>
