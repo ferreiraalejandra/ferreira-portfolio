2fe51e7672fa6be44cb004f58f48e1fb24854afe
ferreiraalejandra <ferreiratorres.alejandra@gmail.com>
2020-05-11 23:53:52 +0200

    state management with redux for NavBar component

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux'
 import './NavBar.css'
 
+const getCurrentTab = state => state.navigation.currentTab
+
 const NavBar = () => {
+  const dispatch = useDispatch()
+  const currentTab = useSelector(getCurrentTab)
+
   return (
     <nav>
       <ul className='navbar-container'>
-        <li className='menu-item'>
-          <Link to="/">Home</Link>
+        <li onClick={() => dispatch({type: 'CHANGE TAB', tabName: 'home'})} className='menu-item'>
+          <Link className={ currentTab === 'home' ? 'underlined' : '' } to="/">Home</Link>
         </li>
-        <li className='menu-item'>
-          <Link to="/about">About</Link>
+        <li onClick={() => dispatch({type: 'CHANGE TAB', tabName: 'about'})} className='menu-item'>
+          <Link className={ currentTab === 'about' ? 'underlined' : '' } to="/about">About</Link>
         </li>
-        <li className='menu-item'>
-          <Link to="/projects">Projects</Link>
+        <li onClick={() => dispatch({type: 'CHANGE TAB', tabName: 'projects'})} className='menu-item'>
+          <Link className={ currentTab === 'projects' ? 'underlined' : '' } to="/projects">Projects</Link>
         </li>
       </ul>
     </nav>
