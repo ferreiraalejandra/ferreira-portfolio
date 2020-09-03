71e4fe5f50375d2b180c2d5f9496614d95acf317
Alejandra Ferreira <alejandra@MacBook-Pro-3.local>
2020-09-03 14:33:40 +0200

    update dependencies and headers

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -8,7 +8,11 @@ const Home = () => {
       <section className='content-section'>
         <div className='hero-image' style={{backgroundImage: `url(${heroImage})`}} >
         <div className='title-container'><span>Ale Ferreira</span></div>
-        <div className='hero-text-container'><span>Full Stack Web Developer<br />Agile enthusiast<br />Driven autodidact<br />Co-founder @MagentaCodes</span></div>
+        <div className='hero-text-container'>
+          <h3>Full Stack Web Developer</h3>
+          <h5>Agile enthusiast<br />Driven autodidact</h5>
+          <h5>Co-founder @MagentaCodes</h5>
+        </div>
         </div>
       </section>
       <section className='content-section'>
